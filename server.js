f40528fd7872df688673623a2ee44bfac31ff50a
RaayeN3 <158215143+RaayeN3@users.noreply.github.com>
2024-03-19 09:59:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-res.write("Hello world!")
+  res.write("Hello world!");
+  res.write("i'm rayen");
   res.end();
 });
 
