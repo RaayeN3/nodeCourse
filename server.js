ef9061c9ee7c2a6bd729fe2396e7e2d7fca1b040
RaayeN3 <158215143+RaayeN3@users.noreply.github.com>
2024-03-19 19:00:15 +0100

    New Update

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require("express");
-
+const bodyParser = require("body-parser").urlencoded({ extended: true });
 const app = express();
 
 // all (get post put patch delete)
-//http request methods
+// http request methods
 
 app.get("/", (req, res, next) => {
-  res.send("<h1>Home page</h1>");
+  res.send(
+    "<form action='/about' method='POST' ><input type='text' name='firstName' /><button type='submit' >Send</button></form>"
+  );
 });
-app.get("/about", (req, res, next) => {
-  res.send("<h1>About page</h1>");
+
+app.post("/about", bodyParser, (req, res, next) => {
+  // let name = [];
+  // req.on("data", (chunk) => {
+  //   name.push(chunk);
+  // });
+  // req.on("end", () => {
+  //   let firstName = Buffer.concat(name).toString();
+  //   console.log(name);
+  //   console.log(firstName);
+  // });
+  res.send("<h1>" + req.body.firstName + "</h1>");
+});
+app.get("/profile/:name", (req, res, next) => {
+  res.send(req.params.name);
 });
 
 app.listen(5000, () => {
