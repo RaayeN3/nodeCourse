cabee56cc0977309830986bd2c137ec4f4e1dbbe
RaayeN3 <158215143+RaayeN3@users.noreply.github.com>
2024-03-19 11:28:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const http = require("http");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  res.write("Hello world!");
-  res.write("i'm rayen");
-  res.end();
+const app = express();
+
+// all (get post put patch delete)
+//http request methods
+
+app.get("/", (req, res, next) => {
+  res.send("<h1>Home page</h1>");
+});
+app.get("/about", (req, res, next) => {
+  res.send("<h1>About page</h1>");
 });
 
-server.listen(5000, () => {
+app.listen(5000, () => {
   console.log("Server is running (port 5000)..");
 });
