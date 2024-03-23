df422bb1dc3d99bd02af8a931a270a7472d131ee
RaayeN3 <158215143+RaayeN3@users.noreply.github.com>
2024-03-23 12:05:11 +0100

    jjjjj

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const { url } = require("inspector");
 const mongo = require("mongodb").MongoClient;
+const mongoose = require("mongoose");
 const app = express();
 
 // const bodyParser = require("body-parser").urlencoded({ extended: true });
 // const bodyParser = express.urlencoded({ extended: true });
 const path = require("path");
+const { connected } = require("process");
 
 //app.set("view engine", "ejs");
 //app.set("views", "views"); // app.set("views",name of the folder (exemple: views) )
@@ -15,16 +18,36 @@ app.use(express.urlencoded({ extended: true })); // using the integrate bodyPars
 // all (get post put patch delete)
 // http request methods
 
-url = "mongodb://localhost:27017/myTest";
+var link = "mongodb://localhost:27017/myTest";
 
 app.get("/home", (req, res, next) => {
-  console.log("connecting..");
-  mongo.connect(url, (err, result) => {
-    if (err) {
-      console.error("Error creating database:", err);
-    } else {
-      console.log("Database created");
-    }
+  mongoose.connect(link).then((result) => {
+    console.log("dabase connected");
+    mongoose.disconnect();
+  });
+  // res.sendFile(__dirname + "\\index.html");
+  // process.platform  output: win32
+
+  // res.sendFile(path.join(__dirname, "views", "index.html"));
+
+  //res.render("index", profile);
+});
+
+app.get("/createCollection", (req, res, next) => {
+  var schemaStudent = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    age: Number,
+  });
+  var Student = mongoose.model("student", schemaStudent);
+  mongoose.connect(link).then((result) => {
+    let s = new Student({
+      firstName: "Rayen",
+      lastName: "Nairi",
+      age: 20,
+    });
+    s.save();
+    //Student.insertMany([{},{}]).then((result)=>{consol.log("Student added")})
   });
   // res.sendFile(__dirname + "\\index.html");
   // process.platform  output: win32
